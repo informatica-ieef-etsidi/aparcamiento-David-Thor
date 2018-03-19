53b055fec12f860db9b92667219351f7f2a6ddef
David-Thor <36692751+David-Thor@users.noreply.github.com>
2018-03-19 20:00:07 +0100

    Segunda parte del ejercicio

--- aparcamiento.c
+++ aparcamiento.c
@@ -1,25 +1,31 @@
 //David Thomas
-//Este programa gestiona dos plazas de aparcamiento: controla si las plazas estan libres o ocupadas y, en ese caso, que coche 
+//Este programa gestiona cuatro plazas de aparcamiento: controla si las plazas estan libres o ocupadas y, en ese caso, que vehiculo
 //esta aparcado y lo que tiene que pagar basandose en el tiempo que lleva
-//El unico problema es que no comprueba si la matricula que se ha introducido es valida, pues tras mucho intentarlo, no lo he logrado
 
 #include<stdio.h>
 #include<time.h>
-#include <windows.h>
+#include<windows.h>
+#include<string.h>
+
+#define NUMPLAZAS 4
+
+struct TPlaza {
+	int vehiculo; //0 - coche, 1 - moto
+	int ocupado; //1 si la plaza esta ocupada
+	char matricula[9];
+};
 
 void main() {        
 
-	//Declaracion de variables
-	int plazaAparcamiento[2] = { 0,0 };        //Las usaré como variables booleanas, 1 si estan ocupadas y 0 si están libres
-	char opcion;
-	char matricula[2][9];
-	int i;
+	struct TPlaza plaza[NUMPLAZAS] = { {0,0}, {0,0}, {1,0}, {1,0} };
+	char opcion, otraOpcion, opcionPlaza, otraMatricula[9];
+	int i, z;
 	float dinero, tiempoTotal;
 
 	while (1) {
 
 		//Pedir datos por pantalla   
-		time_t start_1, start_2, end_1, end_2;  //Creacion de objetos para calcular el tiempo que un coche esta aparcado
+		time_t start[4], end[4];  //Creacion de objetos para calcular el tiempo que un coche esta aparcado
 		SYSTEMTIME str_t;
 		GetSystemTime(&str_t);
 
@@ -28,89 +34,126 @@ void main() {
 		printf("R - Reservar plaza \n");
 		printf("A - Abandonar plaza \n");
 		printf("E - Estado del aparcamiento \n");
+		printf("S - Salir del programa \n");
+		printf("B - Buscar un vehiculo \n");
 		scanf_s("%c", &opcion, 1); getchar();
 
 		switch (opcion) {
 			case 'r':
 			case 'R':
-
-				if (plazaAparcamiento[0] == 0) {
-					plazaAparcamiento[0] = 1;
-					printf("\n Introduce la matricula de tu coche: \n");
-					scanf_s("%s", matricula[0], 8); getchar();
-					printf("Tu coche esta aparcado en la plaza 1");
-					time(&start_1);
-				}
-				else if (plazaAparcamiento[1] == 0) {
-					plazaAparcamiento[1] = 1;
-					printf("\n Introduce la matricula de tu coche: \n");
-					scanf_s("%s", matricula[1], 8); getchar();
-					printf("Tu coche esta aparcado en la plaza 2");
-					time(&start_2);
-				}
-				else printf("\n No hay ninguna plaza libre \n");
-				break;
+				printf("Tienes coche(introduce c) o moto(m) \n");
+				scanf_s("%c", &otraOpcion, 1);
+				switch (otraOpcion)				{
+				case'c':
+				case'C':
+					if (plaza[0].ocupado * plaza[1].ocupado == 0) {
+						for (int j = 0; j < 2; j++) {
+							if (plaza[j].ocupado == 0) {
+								plaza[j].ocupado = 1;
+								printf("Introduce tu matricula: \n"); getchar();
+								gets(plaza[j].matricula);
+								time(&start[j]);
+								printf("Tu coche ha sido aparcado en la plaza %d", j + 1);
+								break;
+							}
+						}
+					}
+					else printf("No quedan plazas libres de este tipo");
+					break;
+				case'm':
+				case'M':
+					if (plaza[2].ocupado * plaza[3].ocupado == 0) {
+						for (int j = 2; j < 4; j++) {
+							if (plaza[j].ocupado == 0) {
+								plaza[j].ocupado = 1;
+								printf("Introduce tu matricula: \n"); getchar();
+								gets(plaza[j].matricula);
+								time(&start[j]);
+								printf("Tu moto ha sido aparcada en la plaza %d", j + 1);
+								break;
+							}
+						}
+					}
+					else printf("No quedan plazas libres de este tipo");
+					break;
+				default:
+					printf("Opcion no valida"); break;
+				}break;
 
 			case 'a':
 			case 'A':
-
-				do {
-					i = 1;
-					printf("\n Introduce la plaza de tu coche (1 o 2) --- pulsa '0' para salir \n");
-					scanf_s("%c", &opcion); getchar();
-					if (opcion == '1') {
-						if (plazaAparcamiento[0] == 1) {
-							plazaAparcamiento[0] = 0;
-							time(&end_1);
-							tiempoTotal = difftime(end_1, start_1);
+				printf("En que plaza esta tu coche? \n");
+				scanf_s("%d", &opcionPlaza);
+				if (1 <= opcionPlaza && opcionPlaza <= 4) {
+					if (plaza[opcionPlaza - 1].ocupado == 1) {
+						printf("Por seguridad, introduce tu matricula: \n"); getchar();
+						gets(otraMatricula);
+						if (strcmp(otraMatricula, plaza[opcionPlaza - 1].matricula) == 0) {   //Si la matricula coincide
+							plaza[opcionPlaza - 1].ocupado = 0;
+							time(&end[opcionPlaza - 1]);
+							tiempoTotal = difftime(end[opcionPlaza - 1], start[opcionPlaza - 1]);
 							dinero = 0.01 * tiempoTotal;               //Por cada segundo se paga 1 cent.
-							printf("Has tenido el coche aparcado %f minutos, vas a tener que pagar %f euros \n", tiempoTotal/60, dinero);
-							scanf_s("%c", &opcion);
+							printf("Has tenido tu vehiculo aparcado %f minutos, vas a tener que pagar %f euros \n", tiempoTotal / 60, dinero);							
 						}
-						else {
-							printf("Esta plaza ya estaba vacia!!, vuelve a intentarlo \n");
-							i = 2;
+						else{          //Si la matricula no coincide 
+							printf("ERROR: Las matriculas no coinciden");
 						}
 					}
-					else if (opcion == '2') {
-						if (plazaAparcamiento[1] == 1) {
-							plazaAparcamiento[1] = 0;
-							time(&end_2);
-							tiempoTotal = difftime(end_2, start_2);
-							dinero = 0.01 * tiempoTotal;
-							printf("Has tenido el coche aparcado %f minutos, vas a tener que pagar %f euros \n", tiempoTotal / 60, dinero);
-							scanf_s("%c", &opcion);
-						}
-						else {
-							printf("Esta plaza ya estaba vacia!!, vuelve a intentarlo \n");
-							i = 2;
-						}
-					}
-					else if (opcion == '0') break;
-					else {
-						i = 2;
-						printf("Opcion no valida, vuelve a intentarlo \n");
-					}
-				} while (i == 2);
+				}
+				else printf("Numero de plaza incorrecto");
+
 				break;
 
 			case 'e':
 			case 'E':
 				printf("plaza 1 -- ");
-				if (plazaAparcamiento[0] == 0) printf("Libre \n"); 
+				if (plaza[0].ocupado == 0) printf("Libre \n"); 
 				else {
 					printf("Ocupada, matricula: ");
-					printf("%s", matricula[0]); printf("\n");
+					printf("%s", plaza[0].matricula); printf("\n");
 				}
 				printf("plaza 2 -- ");
-				if (plazaAparcamiento[1] == 0) printf("Libre \n");
+				if (plaza[1].ocupado == 0) printf("Libre \n");
 				else {
 					printf("Ocupada, matricula: ");
-					printf("%s", matricula[1]); printf("\n");
-				}				
+					printf("%s", plaza[1].matricula); printf("\n");
+				}	
+				printf("plaza 3 -- ");
+				if (plaza[2].ocupado == 0) printf("Libre \n");
+				else {
+					printf("Ocupada, matricula: ");
+					printf("%s", plaza[2].matricula); printf("\n");
+				}
+				printf("plaza 4 -- ");
+				if (plaza[3].ocupado == 0) printf("Libre \n");
+				else {
+					printf("Ocupada, matricula: ");
+					printf("%s", plaza[3].matricula); printf("\n");
+				}
 				break;
 
+			case 's':
+			case 'S':
+				return;
+
+			case 'b':
+			case 'B':
+				printf("Introduce la matricula del vehiculo que quieres buscar");
+				gets(otraMatricula); getchar();
+				z = 0;
+				for (int i = 0; i < NUMPLAZAS; i++) {
+					if (strcmp(otraMatricula, plaza[i].matricula) == 0) {
+						printf("Tu vehiculo esta en la plaza %d", i + 1);
+						z = 1;
+					}
+				}
+				if (z == 0) printf("Esa matricula no se corresponde con la de ningun vehiculo estacionado");
+				break;				
+
+			default:
+				printf("Opcion no valida"); break;
 		}
+		getchar();
 		printf(" \n Pulsa un boton para volver al menu");
 		scanf_s("%c", &opcion);           //Este comando lo unico que hace es mantener la pantalla quieta para el usuario
 		system("cls");                    //pueda leerla comodamente. Al darle a un boton, se limpia y el codigo vuelve a empezar
